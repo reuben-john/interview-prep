caaa394d79fe3f85e6ea5d0571f913e4d46f5b4d
Reuben John <rjkoppy@gmail.com>
2019-08-03 11:01:42 -0500

    Implement reduce

--- performancejs/easy/reduce.js
+++ performancejs/easy/reduce.js
@@ -1,6 +1,15 @@
 // reduce - Implement the reduce function.
+// take in array and reduce it by applying a reducer function to each item in the array
 
-function reduce(array) {}
+function reduce(array, reducer, num) {
+  if (typeof num != "number") {
+    num = 0;
+  }
+  for (let i of array) {
+    num = reducer(num, i);
+  }
+  return num;
+}
 
 import test from "ava";
 
