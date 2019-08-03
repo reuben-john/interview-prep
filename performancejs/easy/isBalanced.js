3b1592afba7d9d2a2bf7803e7ab8b00b3d74452f
Reuben John <rjkoppy@gmail.com>
2019-08-03 15:39:06 -0500

    Implement isBalanced()

--- performancejs/easy/isBalanced.js
+++ performancejs/easy/isBalanced.js
@@ -1,6 +1,24 @@
 // isBalanced - Takes a string and returns true or false indicating whether its curly braces are balanced.
+// opening and closing brackets should always be the same
+// try counting brackets ++ for open, -- for closed
+// if below 0, return false
+// if 0 at end, brakcets are balanced
 
-function isBalanced(array) {}
+function isBalanced(str) {
+  let bracketCount = 0;
+
+  for (let char of str) {
+    if (char == "{") {
+      bracketCount++;
+    } else if (char == "}") {
+      bracketCount--;
+      if (bracketCount < 0) {
+        return false;
+      }
+    }
+  }
+  return bracketCount == 0;
+}
 
 import test from "ava";
 
