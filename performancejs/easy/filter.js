036d560754081c0350f067c34744e52799660534
Reuben John <rjkoppy@gmail.com>
2019-08-03 10:43:11 -0500

    Fix unit test

--- performancejs/easy/filter.js
+++ performancejs/easy/filter.js
@@ -4,4 +4,5 @@ function filter(array, call) {}
 
 import test from "ava";
 
-test("Shoulder filter array", t => t.is([1, 2, 3, 4], n => n < 3), [1, 2]);
+test("Should filter array", t =>
+  t.deepEqual(filter([1, 2, 3, 4], n => n < 3), [1, 2]));
