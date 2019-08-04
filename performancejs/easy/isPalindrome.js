2885568504a4ac9762b18c263b0545eca26f913d
Reuben John <rjkoppy@gmail.com>
2019-08-03 20:35:39 -0500

    Implement isPalindrome

--- performancejs/easy/isPalindrome.js
+++ performancejs/easy/isPalindrome.js
@@ -1,7 +1,20 @@
 // isPalindrome - Return true or false indicating whether the given string is a
 // plaindrone (case and space insensitive).
+// split, removing spaces and go to lowercase, and rejoin
 
-function isPalindrome(array) {}
+function isPalindrome(string) {
+  // remove spaces and make lowercase
+  string = string
+    .toLowerCase()
+    .split(" ")
+    .join("");
+
+  let reversed = string
+    .split("")
+    .reverse()
+    .join("");
+  return reversed == string;
+}
 
 import test from "ava";
 
@@ -10,4 +23,4 @@ test("Odd length palindrome returns true", t =>
   t.is(isPalindrome("abcdcba"), true));
 test("Return false if not palindrome", t => t.is(isPalindrome("abcd"), false));
 test("Long sentence that is palindrome should be true", t =>
-  t.is(isPalindrome("A man a plan and a canal Panama"), true));
+  t.is(isPalindrome("A man a plan a canal Panama"), true));
